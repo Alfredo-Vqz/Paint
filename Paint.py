1d83819e5043852640cdcce2fa67179e47ec981c
Alfredo-vqz <a01656384@tec.mx>
2024-03-21 14:54:29 -0600

    Se agrega la funcion para crear el rectangulo

--- Paint.py
+++ Paint.py
@@ -39,8 +39,19 @@ def draw_circle(start, end):
 
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for _ in range(2):  # Draw two sides
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
